18667f8bbcdb2f07cfde2e3ce9d741e2f0714cb2
dashka-deep <139893307+dashka-deep@users.noreply.github.com>
2026-09-15 14:15:41 +0800

    Update Firebase database URL in config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,10 +4,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAd1vjbLNGkaqV2Y9TbR9CGS_62R72Zff0",
   authDomain: "dashka-96333.firebaseapp.com",
-  // ⚠️ Realtime Database-аа үүсгэсний дараа Firebase console дээрх
-  // Realtime Database хуудасны URL мөрөнд гарч ирэх утгыг энд тавь, жишээ нь:
-  // "https://dashka-96333-default-rtdb.<region>.firebasedatabase.app"
-  databaseURL: "PUT_YOUR_REALTIME_DATABASE_URL_HERE",
+  databaseURL: "https://dashka-96333-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "dashka-96333",
   storageBucket: "dashka-96333.firebasestorage.app",
   messagingSenderId: "488607437519",
